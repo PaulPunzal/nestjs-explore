bf3ae472453c0ddd160d5ef179a13bee54763366
PaulNewbie <pjpunzal@gmail.com>
2026-05-08 10:55:39 +0800

    changing online url

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     path: 'prisma/migrations',
   },
   datasource: {
-    url: process.env.DATABASE_URL ?? 'postgresql://postgres:postgres@localhost:5432/user_crud?schema=public',
-    // url: process.env.DATABASE_URL ?? 'postgresql://user_crud_db_80ru_user:LRMM0hSKOchhE7uOBIOR96K6pTeVF6G6@dpg-d7u5k9jtqb8s739tbsv0-a.oregon-postgres.render.com/user_crud_db_80ru',
+    // url: process.env.DATABASE_URL ?? 'postgresql://postgres:postgres@localhost:5432/user_crud?schema=public',
+    url: process.env.DATABASE_URL ?? 'postgresql://user_crud_db_80ru_user:LRMM0hSKOchhE7uOBIOR96K6pTeVF6G6@dpg-d7u5k9jtqb8s739tbsv0-a.oregon-postgres.render.com/user_crud_db_80ru',
   },
 });
